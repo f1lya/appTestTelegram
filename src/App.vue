a3f4c27e177fe4870f24ccd671c6568384d6d260
Vadim Perfilev <ifff1lya@gmail.com>
2024-06-07 00:29:08 +0300

    fix sass

--- src/App.vue
+++ src/App.vue
@@ -22,20 +22,20 @@ export default {
 
 <style scoped lang="css">
 .button {
-  height: 150px;
-  width: 150px;
+  height: 240px;
+  width: 240px;
   border-radius: 50%;
   background-color: #f4cd2a;
   border: 5px solid #eda323;
-  font-size: 80px;
+  font-size: 140px;
   color: #eda323;
 }
 
 .button:active {
-  margin: 2px;
-  height: 146px;
-  width: 146px;
-  font-size: 76px;
+  margin: 3px;
+  height: 234px;
+  width: 234px;
+  font-size: 136px;
   transition: 2ms;
 }
 </style>
