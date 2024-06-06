70b0f8bffb1cf3e6a0f97f96893d821702562b6d
Vadim Perfilev <ifff1lya@gmail.com>
2024-06-07 00:17:32 +0300

    clicker

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <h2>Clicker MiniApp Telegram</h2>
+    <h3>Score: {{score}}</h3>
+    <input class="button" type="button" value="$" @click="updateScore">
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    score: 0
+  }),
+  methods: {
+    updateScore() {
+      this.score = this.score + 1
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style scoped lang="sass">
+.button
+  height: 150px
+  width: 150px
+  border-radius: 50%
+  background-color: #f4cd2a
+  border: 5px solid #eda323
+  font-size: 80px
+  color: #eda323
+  &:active
+    margin: 2px
+    height: 146px
+    width: 146px
+    font-size: 76px
+    transition: 2ms
 </style>
